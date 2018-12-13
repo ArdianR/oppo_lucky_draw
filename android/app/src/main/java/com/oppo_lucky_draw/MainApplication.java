e160e508910c06812913d4549807ae389a3adf07
ArdianR <fianr5750@gmail.com>
2018-12-13 15:00:27 +0700

    Update MainApplication.java

--- android/app/src/main/java/com/oppo_lucky_draw/MainApplication.java
+++ android/app/src/main/java/com/oppo_lucky_draw/MainApplication.java
@@ -3,6 +3,7 @@ package com.oppo_lucky_draw;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
+import io.invertase.firebase.RNFirebasePackage;
 import codes.simen.IMEI.IMEI;
 import com.learnium.RNDeviceInfo.RNDeviceInfo;
 import com.facebook.react.ReactNativeHost;
@@ -25,6 +26,7 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
+            new RNFirebasePackage(),
             new IMEI(),
             new RNDeviceInfo(),
             new CustomWebViewPackage()
